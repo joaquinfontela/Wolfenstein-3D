6354cbff42be2f2d4545d2d4dd11a026b7904660
Joaquin Fontela <jfontela@fi.uba.ar>
2020-12-21 21:52:24 -0300

    agrego al lector un lector de listas de coordenadas

--- common/src/YAML/YAMLReader.cpp
+++ common/src/YAML/YAMLReader.cpp
@@ -4,7 +4,11 @@ YAMLReader::YAMLReader() { this->fileName = "example.yaml"; }
 
 YAML::Node YAMLReader::getData() { return YAML::LoadFile(this->fileName); }
 
-std::string YAMLReader::getFirstName() {
+std::vector<std::vector<int>> YAMLReader::getId(int id) {
   YAML::Node data = this->getData();
-  return data["first-name"].as<std::string>();
+  std::vector<std::vector<int>> coordinates =
+      data[id].as<std::vector<std::vector<int>>>();
+  return coordinates;
 }
+
+std::vector<std::vector<int>> YAMLReader::getId1s() { return getId(1); }
--- common/src/YAML/example.cpp
+++ common/src/YAML/example.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "../../includes/YAML/YAMLReader.h"
-// compile with flag -lyaml-cpp
 
 int main() {
   YAMLReader yamlReader;
-  std::cout << yamlReader.getFirstName() << std::endl;
+  std::vector<std::vector<int>> coordinates = yamlReader.getId1s();
+  for (std::vector<int> coordinate : coordinates) {
+    std::cout << "(" << coordinate[0] << ", " << coordinate[1] << ")"
+              << std::endl;
+  }
   return 0;
 }
