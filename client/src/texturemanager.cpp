339ecd86b5c5f8b6c8951d8698dc3fbdebf1e64c
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2020-12-26 23:26:02 -0300

    Agrego dibujado de sprites con los ajustes necesarios.

--- client/src/texturemanager.cpp
+++ client/src/texturemanager.cpp
@@ -5,31 +5,36 @@
 
 #include "area.h"
 #include "sdltexture.h"
+#define TEXTURE_LIMIT 64
 
 #define TEXTURE_NOT_FOUND_ERROR "Error, texture not found with code: "
 #define TEXTURE_FOUND_ERROR "Error, texture already found with code: "
+#define TEXTURE_LIMIT_ERROR "Error, cannot add more textures with code: "
 // Cambiar por una excepción.
 
-void TextureManager::render(int img, Area srcArea, Area destArea) {
-  std::map<int, SdlTexture*>::iterator it = this->textures.find(img);
+void TextureManager::render(int id, Area srcArea, Area destArea) {
+  std::map<int, SdlTexture*>::iterator it = this->textures.find(id);
   if (it != this->textures.end())
     it->second->render(srcArea, destArea);
   else
-    std::cerr << TEXTURE_NOT_FOUND_ERROR << img << std::endl;
+    std::cerr << TEXTURE_NOT_FOUND_ERROR << id << std::endl;
 }
 
-void TextureManager::renderAll(int img, Area destArea) {
-  std::map<int, SdlTexture*>::iterator it = this->textures.find(img);
+void TextureManager::renderAll(int id, Area destArea) {
+  std::map<int, SdlTexture*>::iterator it = this->textures.find(id);
   if (it != this->textures.end())
     it->second->renderAll(destArea);
   else
-    std::cerr << TEXTURE_NOT_FOUND_ERROR << img << std::endl;
+    std::cerr << TEXTURE_NOT_FOUND_ERROR << id << std::endl;
 }
 
-void TextureManager::loadTexture(int img, SdlTexture* texture) {
-  std::map<int, SdlTexture*>::iterator it = this->textures.find(img);
+void TextureManager::loadTexture(int id, SdlTexture* texture) {
+  if (this->textures.size() > TEXTURE_LIMIT) {
+    std::cout << TEXTURE_LIMIT_ERROR << std::endl;
+  }
+  std::map<int, SdlTexture*>::iterator it = this->textures.find(id);
   if (it != this->textures.end())
-    std::cerr << TEXTURE_FOUND_ERROR << img << std::endl;
+    std::cerr << TEXTURE_FOUND_ERROR << id << std::endl;
   else
-    this->textures[img] = texture;
+    this->textures[id] = texture;
 }
