96b2f6277cdc2157a60019490112cbe11296b300
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2021-01-05 20:16:24 -0300

    Ajusto archivos.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e "[Installing dependencies]\n"
-echo -e "[Installing YAML-CPP] \n"
+echo -e "\x1b[1m\x1b[32m [Installing dependencies] \x1b[0m\n"
+echo -e "\x1b[1m\x1b[32m [Installing YAML-CPP] \x1b[0m\n"
 
 cd common/src/YAML/yaml-cpp-master
 mkdir build
@@ -11,15 +11,15 @@ sudo make install
 cd ..
 rm -r -f build
 
-echo -e "\n [Compiling Server]\n"
+echo -e "\x1b[1m\x1b[32m \n [Compiling Server] \x1b[0m\n"
 cd ../../../../
 cmake .
 make
 
-echo -e "\n [Compiling Client]\n"
+echo -e "\x1b[1m\x1b[32m \n [Compiling Client] \x1b[0m\n"
 cd client/executable
 cmake .
 make
 
 
-echo -e "\n [Install finished] \n"
+echo -e "\x1b[1m\x1b[32m \n [Install finished] \x1b[0m\n"
