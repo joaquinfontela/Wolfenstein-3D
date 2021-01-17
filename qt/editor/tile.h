221bb50a2fbc61c17ccc24130eaa77db96449e4d
Joaquin Fontela <jfontela@fi.uba.ar>
2021-01-17 16:39:27 -0300

    comienzo con el YAMLWriter

--- qt/editor/tile.h
+++ qt/editor/tile.h
@@ -4,17 +4,17 @@
 #include <QLabel>
 #include <QString>
 
-class tile
-{
-    QLabel label;
-    int type;
-    QImage my_image;
+class tile {
+  QLabel label;
+  int type;
+  QImage my_image;
 
-public:
-    bool cumulative;
-    tile(QString path, int tipo, bool cumulative);
-    QImage get_image();
-    bool accept_tile(tile* tile);
+ public:
+  bool cumulative;
+  tile(QString path, int tipo, bool cumulative);
+  QImage get_image();
+  bool accept_tile(tile* tile);
+  int get_type();
 };
 
-#endif // TILE_H
+#endif  // TILE_H
