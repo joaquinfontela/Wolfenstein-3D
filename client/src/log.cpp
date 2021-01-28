3af926392175c215985fa91f0820d125bca343b2
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2021-01-28 16:49:04 -0300

    Class now writes to a log file.

--- client/src/log.cpp
+++ client/src/log.cpp
@@ -3,16 +3,24 @@
 #include <string>
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <ctime>
 #include <mutex>
 
+#define LOG_FILE_NAME "wolfenstein.log"
+
 void Log::test(const char* _file, const char* function, const int line, const char* msg) {
   static std::mutex mutex;
+
   std::lock_guard<std::mutex> lock(mutex);
   std::string file = std::string(_file);
   file = file.substr(file.find("/src/"));
   std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-  std::cout << std::put_time(localtime(&time), "%T") << " [CLIENT] File: " << file
+
+  std::ofstream logfile;
+  logfile.open(LOG_FILE_NAME, std::ios::app);
+  logfile << std::put_time(localtime(&time), "%T") << " [CLIENT] File: " << file
   << " - Function: " << function << " - Line: " << line << " " << msg << std::endl;
+  logfile.close();
 }
