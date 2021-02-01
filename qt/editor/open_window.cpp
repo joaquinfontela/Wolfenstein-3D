5d3f5bf59e774e1dc15cddbdc3d61758dcb703b5
Joaquin Fontela <jfontela@fi.uba.ar>
2021-02-01 19:31:41 -0300

    borro linea c error de compilacion

--- qt/editor/open_window.cpp
+++ qt/editor/open_window.cpp
@@ -1,38 +1,34 @@
 #include "open_window.h"
-#include "ui_open_window.h"
-#include "tile.h"
+
+#include <fstream>
+
 #include "QHBoxLayout"
 #include "map_save.h"
-#include <fstream>
+#include "tile.h"
+#include "ui_open_window.h"
 
 #define MAP_NAME_PATH "./maps_names.txt"
 
-open_window::open_window(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::open_window)
-{
-    ui->setupUi(this);
-    QVBoxLayout* ly = new QVBoxLayout();
-    ly->setSpacing(0);
-    ly->setWidgetResizable(true);
-    std::ifstream reader;
-    std::string line;
-    reader.open(MAP_NAME_PATH, std::ifstream::in);
-    while (reader.good()) {
-        std::getline(reader, line, '\n');
-        if(line.empty()){
-            continue;
-        }
-        map_save* ms = new map_save(this, line);
-        ly->addWidget(ms,0,Qt::AlignTop);
+open_window::open_window(QWidget* parent)
+    : QDialog(parent), ui(new Ui::open_window) {
+  ui->setupUi(this);
+  QVBoxLayout* ly = new QVBoxLayout();
+  ly->setSpacing(0);
+  std::ifstream reader;
+  std::string line;
+  reader.open(MAP_NAME_PATH, std::ifstream::in);
+  while (reader.good()) {
+    std::getline(reader, line, '\n');
+    if (line.empty()) {
+      continue;
     }
-    reader.close();
+    map_save* ms = new map_save(this, line);
+    ly->addWidget(ms, 0, Qt::AlignTop);
+  }
+  reader.close();
 
-    ui->map_saved_container->setLayout(ly);
-    this->map_selected = NULL;
+  ui->map_saved_container->setLayout(ly);
+  this->map_selected = NULL;
 }
 
-open_window::~open_window()
-{
-    delete ui;
-}
+open_window::~open_window() { delete ui; }
