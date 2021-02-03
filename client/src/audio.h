9edac290c4c82317cc6590db2152cde8ef62663f
Jonathan-R0 <50932273+Jonathan-R0@users.noreply.github.com>
2021-02-03 02:48:12 -0300

    Typo

--- client/src/audio.h
+++ client/src/audio.h
@@ -7,18 +7,6 @@
 
 #include "clientprotocol.h"
 
-/**
- * @brief This union comes in handy due to the fact that each instance
- * of this class is either music or an audio chunk, and since both are pointers
- * I can put them on this structure and dedice how to cast depending on some
- * boolean value.
- *
- */
-union Sound {
-  Mix_Music* music;
-  Mix_Chunk* chunk;
-};
-
 class Audio {
  private:
   int channel;
@@ -73,6 +61,18 @@ class Audio {
    *
    */
   void playWithMaxVolume();
+};
+
+/**
+ * @brief This union comes in handy due to the fact that each instance
+ * of this class is either music or an audio chunk, and since both are pointers
+ * I can put them on this structure and decide how to cast depending on some
+ * boolean value.
+ *
+ */
+union Sound {
+  Mix_Music* music;
+  Mix_Chunk* chunk;
 };
 
 #endif  // AUDIO_H_
