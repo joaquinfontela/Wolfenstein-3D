a269c4a75b1299dbea458054151aedad394fcdee
Joaquin Fontela <jfontela@fi.uba.ar>
2021-02-05 19:00:01 -0300

    soluciono pequeno bug

--- qt/editor/map_actions.cpp
+++ qt/editor/map_actions.cpp
@@ -1,33 +1,35 @@
 #include "map_actions.h"
-#include <vector>
+
 #include <QMouseEvent>
 #include <QPainter>
-#include <iostream>
 #include <QScrollEvent>
+#include <iostream>
+#include <vector>
+
 #include "QCursor"
 #include "QEvent"
-#include "map_scene.h"
-#include "map_painter.h"
 #include "QScrollBar"
+#include "map_painter.h"
+#include "map_scene.h"
 #include "ui_editor.h"
 
-map_actions::map_actions(Editor* editor, map_scene* map_to_paint){
+map_actions::map_actions(Editor* editor, map_scene* map_to_paint) {
   this->editor = editor;
   this->map = map_to_paint;
 }
 
 bool map_actions::eventFilter(QObject* obj, QEvent* event) {
-  if (event->type() == QEvent::MouseButtonPress) {  
+  if (event->type() == QEvent::MouseButtonPress) {
     QMouseEvent* me = (QMouseEvent*)event;
     int x = me->pos().x();
     int y = me->pos().y();
-    if(this->editor->eraser_on == false){
-        add_to_map(x, y);
-    }else{
-        editor->mc->erase_tiles_at(this->get_coordinate(x,y));
+    if (this->editor->eraser_on == false) {
+      add_to_map(x, y);
+    } else {
+      editor->mc->erase_tiles_at(this->get_coordinate(x, y));
     }
     return true;
-  }  else {
+  } else {
     return QObject::eventFilter(obj, event);
   }
 }
@@ -36,12 +38,15 @@ void map_actions::add_to_map(int x, int y) {
   if (editor->tile_item_selected != NULL) {
     std::vector<int> coordinates = get_coordinate(x, y);
     tile_item* tile = this->editor->tile_item_selected;
-    if (this->editor->mc->paint_tile(coordinates, tile)) {        
-      Map_painter* mp = new Map_painter(this->editor->actual_tile_size(), this->map, this->editor->mc);
-      std::vector<tile_item*> tiles_at_coordinates =  this->editor->mc->tiles_at_coordinates(coordinates);
+    if (this->editor->mc->paint_tile(coordinates, tile)) {
+      Map_painter* mp = new Map_painter(this->editor->actual_tile_size(),
+                                        this->map, this->editor->mc);
+      std::vector<tile_item*> tiles_at_coordinates =
+          this->editor->mc->tiles_at_coordinates(coordinates);
       tile->add_to(this->editor->ui->graphics_map_container);
-      mp->paint_map(coordinates,tiles_at_coordinates);
-      this->editor->tile_item_selected = this->editor->tile_item_selected->create_copy();
+      mp->paint_map(coordinates, tiles_at_coordinates);
+      this->editor->tile_item_selected =
+          this->editor->tile_item_selected->create_copy();
     }
   }
 }
@@ -52,8 +57,10 @@ std::vector<int> map_actions::get_coordinate(int x, int y) {
   int COL = this->editor->mc->cant_col;
   int ROW = this->editor->mc->cant_row;
   int tile_size = this->editor->actual_tile_size();
-  QScrollBar* vertical_bar = this->editor->ui->graphics_map_container->verticalScrollBar();
-  QScrollBar* horizontal_bar = this->editor->ui->graphics_map_container->horizontalScrollBar();
+  QScrollBar* vertical_bar =
+      this->editor->ui->graphics_map_container->verticalScrollBar();
+  QScrollBar* horizontal_bar =
+      this->editor->ui->graphics_map_container->horizontalScrollBar();
 
   for (int i = 1; i <= COL; i++) {
     float newColLine = i * tile_size - horizontal_bar->value();
