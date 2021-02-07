dfc9c334ac6a35213dd0d903be12d0f1af960233
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2021-02-07 17:22:15 -0300

    Fix music volume.

--- qt/editor/editor_autogen/EWIEGA46WW/moc_tile_container_actions.cpp
+++ qt/editor/editor_autogen/EWIEGA46WW/moc_tile_container_actions.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'tile_container_actions.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../tile_container_actions.h"
+#include "tile_container_actions.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'tile_container_actions.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.9.5. It"
+#error "This file was generated using the moc from 5.12.8. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -41,7 +41,7 @@ QT_MOC_LITERAL(0, 0, 22) // "tile_container_actions"
 static const uint qt_meta_data_tile_container_actions[] = {
 
  // content:
-       7,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
@@ -62,10 +62,14 @@ void tile_container_actions::qt_static_metacall(QObject *_o, QMetaObject::Call _
     Q_UNUSED(_a);
 }
 
-const QMetaObject tile_container_actions::staticMetaObject = {
-    { &QObject::staticMetaObject, qt_meta_stringdata_tile_container_actions.data,
-      qt_meta_data_tile_container_actions,  qt_static_metacall, nullptr, nullptr}
-};
+QT_INIT_METAOBJECT const QMetaObject tile_container_actions::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_tile_container_actions.data,
+    qt_meta_data_tile_container_actions,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
 
 
 const QMetaObject *tile_container_actions::metaObject() const
