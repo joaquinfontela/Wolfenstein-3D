fa9fcc3aae91c716a7cc844307bf5e6881077357
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2021-02-08 14:58:56 -0300

    Error apendix.

--- client/executable/Wolfen3D_autogen/include/Login_autogen/include/ui_login.h
+++ client/executable/Wolfen3D_autogen/include/Login_autogen/include/ui_login.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'login.ui'
 **
-** Created by: Qt User Interface Compiler version 5.9.5
+** Created by: Qt User Interface Compiler version 5.12.8
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,10 +10,7 @@
 #define UI_LOGIN_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
@@ -38,7 +35,7 @@ public:
     void setupUi(QMainWindow *Login)
     {
         if (Login->objectName().isEmpty())
-            Login->setObjectName(QStringLiteral("Login"));
+            Login->setObjectName(QString::fromUtf8("Login"));
         Login->resize(800, 600);
         QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
         sizePolicy.setHorizontalStretch(0);
@@ -46,38 +43,38 @@ public:
         sizePolicy.setHeightForWidth(Login->sizePolicy().hasHeightForWidth());
         Login->setSizePolicy(sizePolicy);
         Login->setMaximumSize(QSize(800, 600));
-        Login->setStyleSheet(QStringLiteral(""));
+        Login->setStyleSheet(QString::fromUtf8(""));
         centralwidget = new QWidget(Login);
-        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         button_join = new QPushButton(centralwidget);
-        button_join->setObjectName(QStringLiteral("button_join"));
+        button_join->setObjectName(QString::fromUtf8("button_join"));
         button_join->setGeometry(QRect(600, 530, 171, 41));
         QFont font;
-        font.setFamily(QStringLiteral("Ubuntu Condensed"));
+        font.setFamily(QString::fromUtf8("Ubuntu Condensed"));
         font.setPointSize(18);
         font.setBold(true);
         font.setWeight(75);
         button_join->setFont(font);
         dato_ip = new QLineEdit(centralwidget);
-        dato_ip->setObjectName(QStringLiteral("dato_ip"));
+        dato_ip->setObjectName(QString::fromUtf8("dato_ip"));
         dato_ip->setGeometry(QRect(600, 330, 171, 41));
         dato_puerto = new QLineEdit(centralwidget);
-        dato_puerto->setObjectName(QStringLiteral("dato_puerto"));
+        dato_puerto->setObjectName(QString::fromUtf8("dato_puerto"));
         dato_puerto->setGeometry(QRect(600, 380, 171, 41));
         dato_game = new QLineEdit(centralwidget);
-        dato_game->setObjectName(QStringLiteral("dato_game"));
+        dato_game->setObjectName(QString::fromUtf8("dato_game"));
         dato_game->setGeometry(QRect(600, 430, 171, 41));
         button_create = new QPushButton(centralwidget);
-        button_create->setObjectName(QStringLiteral("button_create"));
+        button_create->setObjectName(QString::fromUtf8("button_create"));
         button_create->setGeometry(QRect(600, 480, 171, 41));
         button_create->setFont(font);
         Login->setCentralWidget(centralwidget);
         menubar = new QMenuBar(Login);
-        menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 800, 22));
         Login->setMenuBar(menubar);
         statusbar = new QStatusBar(Login);
-        statusbar->setObjectName(QStringLiteral("statusbar"));
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
         Login->setStatusBar(statusbar);
 
         retranslateUi(Login);
@@ -87,9 +84,9 @@ public:
 
     void retranslateUi(QMainWindow *Login)
     {
-        Login->setWindowTitle(QApplication::translate("Login", "Login", Q_NULLPTR));
-        button_join->setText(QApplication::translate("Login", "JOIN MATCH", Q_NULLPTR));
-        button_create->setText(QApplication::translate("Login", "CREATE MATCH", Q_NULLPTR));
+        Login->setWindowTitle(QApplication::translate("Login", "Login", nullptr));
+        button_join->setText(QApplication::translate("Login", "JOIN MATCH", nullptr));
+        button_create->setText(QApplication::translate("Login", "CREATE MATCH", nullptr));
     } // retranslateUi
 
 };
