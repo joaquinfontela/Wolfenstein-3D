141e827c1b7ca69b7725106c6712fa292e2a8147
thiagokovnat <tkovnat@fi.uba.ar>
2021-02-09 17:56:14 -0300

    fix yaml config reader

---
+++ client/includes/configReader.h
@@ -0,0 +1,19 @@
+#ifndef __CONFIG_READER_H__
+#define __CONFIG_READER_H__
+#include <yaml-cpp/yaml.h>
+
+
+class ConfigReader{
+
+private:
+  std::string fileName;
+  YAML::Node data;
+  YAML::Node getData();
+
+public:
+    ConfigReader(std::string fileName);
+    int getWidth();
+    int getHeight();
+};
+
+#endif
