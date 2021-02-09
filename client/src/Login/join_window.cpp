c0ffd391e06fcebf2aafb1edf591846cf9aee332
Joaquin <jbetz@fi.uba.ar>
2021-02-09 14:28:43 -0300

    corrijo problemas memoria login y agrego logo a la ventana

--- client/src/Login/join_window.cpp
+++ client/src/Login/join_window.cpp
@@ -11,15 +11,28 @@ join_window::join_window(QWidget* parent, std::vector<int> availableMatches)
   ui->setupUi(this);
   QVBoxLayout* ly = new QVBoxLayout();
   ly->setSpacing(10);
+  bool is_first_iteration = true;
+  match_id* last_mi;
   for (size_t i = 0; i < availableMatches.size(); i++) {
     match_id* new_match_id = new class match_id(this, availableMatches.at(i));
-    ly->addWidget(new_match_id, 0, Qt::AlignTop);
+    if(is_first_iteration){
+        QObject::connect(ly, &QObject::destroyed, new_match_id, &QObject::deleteLater);
+        last_mi = new_match_id;
+        is_first_iteration = false;
+     }else{
+        QObject::connect(last_mi, &QObject::destroyed, new_match_id, &QObject::deleteLater);
+        last_mi = new_match_id;
+     }
+     ly->addWidget(new_match_id, 0, Qt::AlignTop);
   }
   ui->id_container->setLayout(ly);
   my_match_id = -1;
 }
 
-join_window::~join_window() { delete ui; }
+join_window::~join_window() {
+    delete ui->id_container->layout();
+    delete ui;
+}
 
 uint32_t join_window::get_match_id() { return (uint32_t)this->my_match_id; }
 
