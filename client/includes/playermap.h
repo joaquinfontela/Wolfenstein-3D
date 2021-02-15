453bb4a054e308be2e5cb8a606fa7920133b7afb
Jonathan-R0 <50932273+Jonathan-R0@users.noreply.github.com>
2021-02-15 01:50:24 -0300

    Documentation++

--- client/includes/playermap.h
+++ client/includes/playermap.h
@@ -4,15 +4,37 @@
 #include "player.h"
 #include <map>
 
+/**
+ * @brief Dictionary with all the players, mapped by their id's.
+ *
+ */
 class PlayerMap {
  private:
   Player* self;
+ 
+  /**
+  * @brief Deallocates all the resources.
+  *
+  */
   void garbageCollector();
  public:
   std::map<uint32_t, Player*> dict;
   PlayerMap(uint32_t selfId);
   ~PlayerMap();
+ 
+  /**
+  * @brief Verifies if the given player is included in the map.
+  *
+  * @param id Player id.
+  * @returns true if the given player is in the map.
+  */
   bool isContained(uint32_t id);
+ 
+  /**
+  * @brief Returns a pointer to the one's player. Must not be deleted.
+  *
+  * @returns player Reference to one's player.
+  */
   Player* getSelf();
 };
 
