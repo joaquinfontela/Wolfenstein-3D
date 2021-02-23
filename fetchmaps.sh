fa5ef17df79bd12e8bf6b1d4134fd3a7cf063096
thiagokovnat <tkovnat@fi.uba.ar>
2021-02-23 17:48:21 -0300

    update fetch script

--- fetchmaps.sh
+++ fetchmaps.sh
@@ -1,3 +1,28 @@
-svn export https://github.com/joaquinfontela/tp-final-taller/trunk/common/src/YAML/Maps
-sudo mv Maps/* usr/local/share/Wolfenstein/YAML/Maps
-rm -r Maps
+function DownloadMaps(){
+  svn export https://github.com/joaquinfontela/tp-final-taller/trunk/common/src/YAML/Maps
+  sudo mv Maps/* /usr/local/share/Wolfenstein/YAML/Maps
+  rm -r Maps
+}
+
+function moveMaps(){
+  sudo cp qt/editor/maps/*.yaml /usr/local/share/Wolfenstein/YAML/Maps
+}
+
+case $1 in
+-a)
+  DownloadMaps
+  moveMaps
+  ;;
+-d)
+  DownloadMaps
+  ;;
+-m)
+  moveMaps
+  ;;
+-h)
+  echo -e "Help:
+  -a -> Download official maps and move user-created maps to media folder
+  -d -> Only download official maps
+  -m -> Only moves user-created maps to media folder"
+  ;;
+esac
